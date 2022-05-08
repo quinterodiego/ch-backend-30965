978f07e9ef6ab9d4a570bd0f6ccea47e2d057ccf
quinterodiego <d86webs@gmail.com>
2022-05-07 21:01:59 -0300

    desafio clase 04

--- Container.js
+++ Container.js
@@ -7,39 +7,59 @@ class Container {
     }
 
     save = async ( product ) => {
-        const data = await fs.promises.readFile( this.path, 'utf-8' )
-        const dataArray = JSON.parse( data )
-        const id = dataArray.length + 1
-        const newProduct = { ...product, id }
-        dataArray.push( newProduct )
-        const newProductJSON = JSON.stringify( dataArray, null, 2 )
-        await fs.promises.writeFile( this.path, newProductJSON )
-        console.log( "ID asignado: ", id )
+        try {
+            const data = await fs.promises.readFile( this.path, 'utf-8' )
+            const dataArray = JSON.parse( data )
+            const id = dataArray.length + 1
+            const newProduct = { ...product, id }
+            dataArray.push( newProduct )
+            const newProductJSON = JSON.stringify( dataArray, null, 2 )
+            await fs.promises.writeFile( this.path, newProductJSON )
+            console.log( "ID asignado: ", id )
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     getById = async ( id ) => {
-        const data = await fs.promises.readFile( this.path, 'utf-8' )
-        const dataArray = JSON.parse( data )
-        const product = dataArray.find(p => p.id = id)
-        console.log(product)
+        try {
+            const data = await fs.promises.readFile( this.path, 'utf-8' )
+            const dataArray = JSON.parse( data )
+            const product = dataArray.find(p => p.id == id)
+            product ? console.log("Producto encontrado: ", product) : console.log(null)
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     getAll = async () => {
-        const data = await fs.promises.readFile( this.path, 'utf-8' )
-        const dataArray = JSON.parse( data )
-        console.log( "Data: ", dataArray )
+        try {
+            const data = await fs.promises.readFile( this.path, 'utf-8' )
+            const dataArray = JSON.parse( data )
+            console.log( "Data: ", dataArray )
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     deleteById = async ( id ) => {
-        const data = await fs.promises.readFile( this.path, 'utf-8' )
-        const dataArray = JSON.parse( data )
-        const products = dataArray.filter((p => p.id !== id))
-        const newProducts = JSON.stringify( products, null, 2 )
-        await fs.promises.writeFile( this.path, newProducts )
+        try {
+            const data = await fs.promises.readFile( this.path, 'utf-8' )
+            const dataArray = JSON.parse( data )
+            const products = dataArray.filter((p => p.id !== id))
+            const newProducts = JSON.stringify( products, null, 2 )
+            await fs.promises.writeFile( this.path, newProducts )
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     deleteAll = async () => {
-        await fs.promises.writeFile( this.path, '[]' )
+        try {
+            await fs.promises.writeFile( this.path, '[]' )
+        } catch (error) {
+            console.error(error)
+        }
     }
 }
 
@@ -52,7 +72,7 @@ const product = {
 }
 
 // c.save( product );
-// c.getById( 2 );
+ c.getById( 1);
 // c.getAll();
 // c.deleteById(2);
 // c.deleteAll();
