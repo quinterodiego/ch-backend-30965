4d132212607ca74abe7cafcb3f1db9e2fd4627e3
quinterodiego <d86webs@gmail.com>
2022-05-24 17:45:05 -0300

    termino desafio parte ejs

--- Server.js
+++ Server.js
@@ -7,7 +7,7 @@ const app = express()
 const contenedor = new Container();
 
 app.set('views', './views')
-app.set('view engine', 'pug')
+app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: 'true' }))
 
