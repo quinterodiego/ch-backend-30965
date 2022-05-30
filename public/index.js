1e467116329cf2e1782834cbee78a50b1963ad0c
quinterodiego <d86webs@gmail.com>
2022-05-30 00:17:41 -0300

    casi termino el ejercicio

---
+++ public/index.js
@@ -0,0 +1,23 @@
+const socket = io()
+
+const input = document.getElementById('message')
+const button = document.getElementById('send')
+
+button.addEventListener('click', () => {
+    socket.emit('mensajeCliente', input.value)
+})
+
+socket.on('mensajeServidor', (data) => {
+    const message = `<br>SocketId: ${data.socketID} --> Mensaje: ${data.mensaje}`
+    document.querySelector('p').innerHTML += message
+})
+
+socket.on('mensajes', data => {
+    const messages = data
+        .map(message => {
+        `SocketId: ${message.socketID} --> Mensaje: ${message.mensaje}`
+    })
+        .join('<br>')
+
+    document.querySelector('p').innerHTML += messages
+})
